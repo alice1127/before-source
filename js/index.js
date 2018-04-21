49274e2a5c9360b18f34244a1b4bf3d26721781b
alice1127 <37846391+alice1127@users.noreply.github.com>
2018-04-21 22:38:11 +0800

    Add files via upload
    original source

--- js/index.js
+++ js/index.js
@@ -1,8 +1,6 @@
 let rect = document.getElementById("rect");
-let rect1 = document.getElementById("rect1");
 let username = document.getElementById("username");
 let password = document.getElementById("password");
-
 
 function handle1() {
 	rect.setAttribute("class", "rect2");
@@ -10,15 +8,6 @@ function handle1() {
 
 function handle2() {
 	rect.setAttribute("class", "rect1");
-}
-
-function handle3() {
-	if (username.value.length > 0 && password.value.length > 0) {
-		rect1.setAttribute("class", "rect3 animate");
-		setTimeout(function() {
-			rect1.setAttribute("class", "");
-		}, 900);
-	}
 }
 
 //For codepen header!!!
